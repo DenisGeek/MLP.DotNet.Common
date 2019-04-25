eac1487121c46adc14aad04ad15d01758b0917e5
DenisGeek <spam@denis.tk>
2019-04-25 14:14:56 +0200

    MLP.Tools.Data.TreeStructure = Added example of Tree data Structure

---
+++ MLP/Tools/Data/TreeStructure/MLP.Tools.Data.TreeStructure/NodeFromJson_Ext.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLP.Tools
+{
+    public static class NodeFromJson_Ext
+    {
+        public static Node Json2Node(this string the)
+            => (Node)JsonConvert.DeserializeObject(the);
+    }
+}
---
+++ MLP/Tools/Data/TreeStructure/MLP.Tools.Data.TreeStructure/NodeToJson_Ext.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLP.Tools
+{
+    public static class NodeToJson_Ext
+    {
+        public static string ToJson(this Node the)
+          => JsonConvert.SerializeObject(the, Formatting.Indented);
+    }
+}
