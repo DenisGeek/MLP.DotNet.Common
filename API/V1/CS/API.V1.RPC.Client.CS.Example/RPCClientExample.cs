4e3618cfdd2dc8643c38a530f55e243c32f912ac
DenisGeek <spam@denis.tk>
2019-05-02 19:46:21 +0200

    added API.V1.RPC.Client.CS.Example

---
+++ API/V1/CS/API.V1.RPC.Client.CS.Example/RPCClientExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using API.V1.RPC;
+
+namespace API.V1.RPC.Client.CS.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var message = "1";
+            using (var aReguest = new RpcClient())
+            {
+                Console.WriteLine($" [x] Requesting {message})");
+                var response = aReguest.Call(message);
+                Console.WriteLine($" [.] Got {response}");
+            }
+        }
+    }
+}
