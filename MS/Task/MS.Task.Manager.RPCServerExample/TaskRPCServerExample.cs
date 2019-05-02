d1923b7d78ff583398198967434a7a3265d1a6fc
DenisGeek <spam@denis.tk>
2019-05-02 19:47:38 +0200

    added MS.Task.Manager.RPCServerExample

---
+++ MS/Task/MS.Task.Manager.RPCServerExample/TaskRPCServerExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using API.V1.RPC;
+using MLP.Tools;
+
+namespace MS.Task.Manager.RPCServerExample
+{
+    class TaskRPCServerExample
+    {
+        private static RpcServer _server;
+
+        static void Main(string[] args)
+        {
+            InitServer();
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static void InitServer()
+        {
+            _server = new RpcServer();
+            _server.HandlerReceivedJson = MessageHandler;
+            Console.WriteLine(" [x] Awaiting RPC requests");
+        }
+
+        private static string MessageHandler(string aMessage)
+        {
+            var tree = aMessage.Json2Node<string>();
+            tree.AddChild(" :metal: Branch From MS.Task.Manager.RPCServerExample");
+
+            Console.WriteLine(" [x] Branch has been added");
+
+            return tree.ToJson();
+        }
+    }
+}
