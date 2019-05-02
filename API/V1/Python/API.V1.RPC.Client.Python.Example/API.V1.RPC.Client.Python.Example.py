f75993cca368c000cce2ef142d206cad234a4382
DenisGeek <spam@denis.tk>
2019-05-02 14:30:25 +0200

    Python RpcClient example tested using "with" directive

--- API/V1/Python/API.V1.RPC.Client.Python.Example/API.V1.RPC.Client.Python.Example.py
+++ API/V1/Python/API.V1.RPC.Client.Python.Example/API.V1.RPC.Client.Python.Example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from RpcClient import RpcClient
 
-rpcClient = RpcClient()
-print(" [x] Requesting fib(30)")
-response = rpcClient.call("2")
-print(" [.] Got %r" % response)
+with RpcClient() as rpcClient:
+    print(" [x] Requesting fib(30)")
+    response = rpcClient.call("2")
+    print(" [.] Got %r" % response)
