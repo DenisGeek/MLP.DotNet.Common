107d6130f5b4123cadd3b5084ed2f1e12767f256
DenisGeek <spam@denis.tk>
2019-05-02 13:29:52 +0200

    Python WIP

---
+++ API/V1/CS/API.V1.RPC.Server.CS.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace API.V1.RPC.Server.CS.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
