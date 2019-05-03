f2f7188d69a49920d3087c9e98ea1ed01dd2fdb9
DenisGeek <spam@denis.tk>
2019-05-03 12:02:02 +0200

    added EnvironmentExt due no extension, no inherit a static class EnvironmentExt.GetEnvironmentVariable with def val

---
+++ Tools/Environment/Tools.EnvironmentExt/EnvironmentExt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tools.Environment
+{
+    public static class EnvironmentExt
+    {
+        public static string GetEnvironmentVariable(string variable, string defValue)
+        {
+            var value = Environment.GetEnvironmentVariable(variable);
+            //check for a value, if nothing is returned then def val
+            if (!string.IsNullOrEmpty(value))
+                return value;
+            else
+                return defValue;
+        }
+    }
+}
