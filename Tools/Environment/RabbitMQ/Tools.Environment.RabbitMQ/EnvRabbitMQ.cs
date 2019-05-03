f6459e7350338e19a1c676558f8c0084c1eb9e0f
DenisGeek <spam@denis.tk>
2019-05-03 12:02:59 +0200

    Added dll for get environment variables 4 RabbitMQ

---
+++ Tools/Environment/RabbitMQ/Tools.Environment.RabbitMQ/EnvRabbitMQ.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tools.Environment
+{
+    public static class EnvRabbitMQ
+    {
+        public static readonly string User;
+        public static readonly string Pass;
+        public static readonly string Host;
+        public static readonly int Port;
+        static EnvRabbitMQ()
+        {
+            User = EnvironmentExt.GetEnvironmentVariable("RabbitMQ_User", "guest");
+            Pass = EnvironmentExt.GetEnvironmentVariable("RabbitMQ_Pass", "guest");
+            Host = EnvironmentExt.GetEnvironmentVariable("RabbitMQ_Host", "localhost");
+            Port = Convert.ToInt32(EnvironmentExt.GetEnvironmentVariable("RabbitMQ_Port", "5672"));
+        }
+    }
+}
