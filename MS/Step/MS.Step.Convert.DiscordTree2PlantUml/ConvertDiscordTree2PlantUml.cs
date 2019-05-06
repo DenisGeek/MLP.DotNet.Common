9f0803de500e9077893cbc476be1605f3c254f9e
DenisGeek <mail@denis.tk>
2019-05-06 08:32:43 +0200

    added MS.Step.Convert.DiscordTree2PlantUml

---
+++ MS/Step/MS.Step.Convert.DiscordTree2PlantUml/ConvertDiscordTree2PlantUml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MLP.Tools;
+
+namespace MS.Step.Convert.DiscordTree2PlantUml
+{
+    public class ConvertDiscordTree2PlantUml : IDisposable
+    {
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public string Do(string aMessage)
+        {
+            var res = "";
+            res += header();
+            res += body(aMessage);
+            res += footer();
+
+            return res;
+        }
+
+        public string header()
+            => "@startuml\nsalt\n{\n  {T\n";
+        //\nskinparam backgroundColor black
+        public string footer()
+            => "  }\n}\n@enduml";
+
+        private enum dscTree { Category, Channel }
+
+        private string AddNode(int aLevel, string aNodeName, string aNodeDescr, dscTree aNodeKind)
+        {
+            string GetNodeColor(dscTree inner_aNodeKind)
+            {
+                switch (aNodeKind)
+                {
+                    case dscTree.Category:
+                        return "<color:blue>";
+                    case dscTree.Channel:
+                        return "<color:green>";
+                    default:
+                        return "<color:black>";
+                }
+            }
+            var nodeColor = GetNodeColor(aNodeKind);
+
+            var res = "    ";
+            for (var i = 0; i < aLevel; i++)
+                res += "+";
+            
+            aNodeDescr = string.IsNullOrEmpty(aNodeDescr) ? "" : $" | {aNodeDescr}";
+            res += $" {nodeColor}  {aNodeName} { aNodeDescr }\n";
+
+            return res;
+        }
+
+        class DiscordNode
+        {
+            public string NodePName;
+            public string NodePKind;
+            public string NodePDescr;
+        }
+
+        private string body(string aMessage)
+        {
+            var tree = aMessage.Json2Node<DiscordNode>();
+            string Childs(int inner_Level, Node<DiscordNode> inner_Node)
+            {
+                var inner_res = AddNode(inner_Level, inner_Node.Data.NodePName, inner_Node.Data.NodePDescr, dscTree.Channel);
+                foreach (var inner_childNode in inner_Node.Children)
+                    inner_res += Childs(inner_Level + 1, inner_childNode);
+                return inner_res;
+            }
+            var res = Childs(1, tree);
+
+            return res;
+        }
+    }
+}
---
+++ MS/Step/MS.Step.Convert.DiscordTree2PlantUml/mainStepConvertDiscordTree2PlantUml.cs
@@ -0,0 +1,50 @@
+﻿using API.V1.RPC;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Tools.Environment;
+
+namespace MS.Step.Convert.DiscordTree2PlantUml
+{
+    class Program
+    {
+        private static RpcServer _server4IncomingMessages;
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+
+            InitServer4IncomingMessages();
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        static int ifile = 0;
+        private static string MessageHandler(string aMessage)
+        {
+            //ifile++;
+            Console.WriteLine($"MS.Step.Convert.DiscordTree2PlantUml\n => [x] Recieved:  {aMessage}");
+            File.WriteAllText($"{ifile}.MS.Step.Convert.DiscordTree2PlantUml.In.txt", aMessage);
+
+            var res = new ConvertDiscordTree2PlantUml().Do(aMessage);
+
+            Console.WriteLine($"MS.Step.Convert.DiscordTree2PlantUml\n <= [x] Reply with:  {res}");
+            File.WriteAllText($"{ifile}.MS.Step.Convert.DiscordTree2PlantUml.Out.txt", res);
+
+            return res;
+        }
+
+        private static void InitServer4IncomingMessages()
+        {
+            _server4IncomingMessages = new RpcServer(
+                                    aHostName: EnvRabbitMQ.Host,
+                                    aVirtualHost: EnvRqabbitMQStepConvertDiscordTree2PlantUml.VirtualHost,
+                                    aPort: EnvRabbitMQ.Port,
+                                    aQueueName: EnvRqabbitMQStepConvertDiscordTree2PlantUml.QueueName,
+                                    aUser: EnvRabbitMQ.User,
+                                    aPass: EnvRabbitMQ.Pass
+                                    );
+            _server4IncomingMessages.HandlerReceivedJson = MessageHandler;
+            Console.WriteLine("MS.Step.Convert.DiscordTree2PlantUml\n [x] Awaiting RPC requests");
+        }
+    }
+}
