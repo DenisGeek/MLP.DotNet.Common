a161ad1dbc24b5fdee46d43af602c77c2801eb8d
DenisGeek <spam@denis.tk>
2019-05-10 12:46:01 +0200

    Added Helpers.Class.Object

---
+++ Tools/Helpers/Helpers.Class/ClassObjectExt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Helpers.Class.Object
+{
+    public static class ClassObjectExt
+    {
+        public static string GetMethodName(this object the)
+            => new StackTrace().GetFrame(1).GetMethod().Name;
+
+        public static string GetNamespace(this object the)
+            => System.Reflection.Assembly.GetCallingAssembly().EntryPoint.DeclaringType.Namespace;
+    }
+}
